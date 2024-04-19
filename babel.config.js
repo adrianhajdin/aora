fd4bb13eb256daa820cb9ec2a959680a35eb244a
unknown <herbertntim15@gmail.com>
2024-04-19 04:43:51 +0000

    setting up environment variables

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
+    plugins: ["nativewind/babel", "react-native-reanimated/plugin",
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
   };
 };
